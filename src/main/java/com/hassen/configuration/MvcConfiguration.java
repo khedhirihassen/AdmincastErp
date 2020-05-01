402c88cd15be70dd90e07ac8b075a26ed544edc6
Hassen Khdhiri <hassen.khedhiri@outlook.com>
2020-05-01 18:20:50 +0100

    Configuration initiale de projet Spring Boot - JPA-Maven-MySQL-Thymleaf

---
+++ src/main/java/com/hassen/configuration/MvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.hassen.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+	@Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+        viewControllerRegistry.addViewController("/").setViewName("redirect:/index");
+    }
+	
+}
