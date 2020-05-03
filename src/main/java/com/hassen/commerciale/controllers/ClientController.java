bfdbb3b24dec7ff0371adec13c2ef984c14482f7
Hassen Khdhiri <hassen.khedhiri@outlook.com>
2020-05-03 01:08:35 +0100

    Ajout de methode delete pour le module client

--- src/main/java/com/hassen/commerciale/controllers/ClientController.java
+++ src/main/java/com/hassen/commerciale/controllers/ClientController.java
@@ -4,23 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hassen.commerciale.services.ClientService;
 
 @Controller
-@RequestMapping ("clients")
+@RequestMapping("clients")
 public class ClientController {
 
 	@Autowired
 	private ClientService clientService;
-	
+
 	@GetMapping
 	public String showClients(Model model) {
-		
-		model.addAttribute("clients",clientService.getAllClients());
+
+		model.addAttribute("clients", clientService.getAllClients());
 		return "clients/listeClients";
-		
+
 	}
-	
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Long id) {
+
+		clientService.delete(id);
+		return "redirect:/clients";
+	}
+
 }
