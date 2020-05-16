740fea3aed40dcbaa7f5ca9371bd909e79b8693e
Hassen Khdhiri <hassen.khedhiri@outlook.com>
2020-05-17 00:47:08 +0100

    ajouter les modules d'ajout et de modification de module Client

--- src/main/java/com/hassen/commerciale/controllers/ClientController.java
+++ src/main/java/com/hassen/commerciale/controllers/ClientController.java
@@ -1,12 +1,17 @@
 package com.hassen.commerciale.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hassen.commerciale.entities.Client;
 import com.hassen.commerciale.services.ClientService;
 
 @Controller
@@ -23,6 +28,46 @@ public class ClientController {
 		return "clients/listeClients";
 
 	}
+	
+	@GetMapping("addClient")
+    public String showAddForm(Client client) {
+		
+		return "clients/addClient";
+
+    }
+	
+	@PostMapping("add")
+	public String addStudent(@Valid Client client, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "clients/addClient";
+		}
+
+		clientService.save(client);
+		return "redirect:/clients";
+	}
+	
+	@GetMapping("edit/{id}")
+	public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+		Client client = clientService.getClientById(id);
+		model.addAttribute("client", client);
+		return "clients/updateClient";
+	}
+
+	@PostMapping("update/{id}")
+	public String updateClient(@PathVariable("id") Long id, @Valid Client client, BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			client.setId(id);
+			return "clients/updateClient";
+		}
+
+		clientService.update(client);
+		//model.addAttribute("clients", clientService.getAllClients());
+		return "redirect:/clients";
+	}
+
+	
+	
 
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Long id) {
